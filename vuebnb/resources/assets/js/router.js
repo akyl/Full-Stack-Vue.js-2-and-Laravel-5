3e4d99b09acf0223af96b77c70cdfef1fa25a7ce
Akyl Zholonov <zholonov@gmail.com>
2018-02-01 23:46:48 +0600

    add authentication

--- vuebnb/resources/assets/js/router.js
+++ vuebnb/resources/assets/js/router.js
@@ -43,6 +43,7 @@ router.beforeEach((to, from, next) => {
   }
   else {
     store.commit('addData', {route: to.name, data: serverData});
+    serverData.saved.forEach(id => store.commit('toggleSaved', id));
     next();
   }
 });
